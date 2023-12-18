77980905fd9aa8815457689c6700a68b7928c623
Abhishek Sagar <sachinites@gmail.com>
2023-12-18 17:42:14 +0530

    Added Operational Address.

--- design/include/footer.php
+++ design/include/footer.php
@@ -18,7 +18,7 @@
             <div class="row align-items-center">
                 <div class="col-sm-7">
                 <p>
-© Copyright 2022 | Csepracticals. | All Rights Reserved
+© Copyright 2023 | Csepracticals. | All Rights Reserved | Bangalore, India - 560103
 
 </p>
                 </div>
