bc24accb35f21f3756132fec27eff8a07d68cc29
Abhishek Sagar <sachinites@gmail.com>
2023-12-20 14:24:46 +0530

    Removed affiliate link.

--- application/views/site/index.php
+++ application/views/site/index.php
@@ -67,8 +67,8 @@
                             Welcome to <?= $this->config->item('PROJECT'); ?>, an Online Course offering Website in the
                             field of Operating Systems, Networking, Linux System Programming and Several Coding
                             Projects. We offer only Development based Projects, no DS/ALGO/CP.
-                            <br><br>
-                            <h3><a href = "https://www.youtube.com/watch?v=h84iyzqa8Sc" target="_blank">New : I want to Join as Affiliate</a></h3>
+                           <!-- <br><br>
+                            <h3><a href = "https://www.youtube.com/watch?v=h84iyzqa8Sc" target="_blank">New : I want to Join as Affiliate</a></h3>  -->
                         </p>
                     </div>
                 </div>
